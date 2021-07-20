9600a3e7802999c013607af261a61be4e588ad4e
andrejarboe <andrejarboe@gmail.com>
2021-07-20 12:16:45 -0500

    start game logic

---
+++ public/js/components/regularJS.js
@@ -0,0 +1,66 @@
+var gameState = {
+  userPokemon: "",
+  rivalPokemon: "",
+};
+
+var pokemonsEL = document
+  .querySelector(".select-screen")
+  .querySelectorAll(".character");
+
+console.log(pokemonsEL);
+
+var i = 0;
+while (i < pokemonsEL.length) {
+  pokemonsEL[i].onclick = function () {
+    var pokemonName = this.dataset.userPokemon;
+    gameState.userPokemon = pokemonName;
+    console.log(gameState);
+  };
+  i++;
+}
+
+function
+
+// webpackJsonp([0],{
+
+// /***/ 235:
+// /***/ (function(module, exports, __webpack_require__) {
+
+// "use strict";
+
+// // pokemon
+// // create data for 3 different pokemons, with their names, type, weaknesses, health, and attack moves(name, attack stat, maximum)
+// var pokemons = [{
+//   name: 'charmander',
+//   type: 'fire',
+//   attack: 52,
+//   stamina: 39,
+//   level: 1
+// }, {
+//   name: 'charmander',
+//   type: 'fire',
+//   attack: 52,
+//   stamina: 39,
+//   level: 1
+// }];
+
+// var attack = 20;
+// var level = 10;
+// var stack = 1.3;
+// var stamina = 39;
+
+// // create a formula for attacks
+// console.log(attack * level * stack / 7);
+
+// // create a formula for health
+// //HP = 0.20 x Sqrt(Pokemon_level) x (HP_base_stat)
+// console.log(0.20 * Math.sqrt(level) * stamina * 15);
+
+// // let user choose 1 and then assign a random pokemon to battle thats not the users pokemon
+// // p1 vs p2
+
+// // when one user loses all his health declare a winner
+
+// /***/ })
+
+// },[235]);
